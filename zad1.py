b47414847ae0562c047a12acfc84d4d0c9f864fb
Krzysztof Kosinski <drakerc@me.com>
2018-10-03 22:41:53 +0200

    Add the possibility to draw lines

--- zad1.py
+++ zad1.py
@@ -1,60 +1,109 @@
+from functools import partial
 from tkinter import *
 
-canvas_width = 500
-canvas_height = 150
-
-
 class Menu():
+    root = Tk()
+    w = Canvas(root,
+               width=700,
+               height=600,
+               background='#ffffff')
+
+    mode = 'none'
+    mouse_line_start = None
+    mouse_line_end = None
+
     def __init__(self):
         maincolor = '#0288d1'
-        global root
-        root = Tk()
-        root.configure(bg=maincolor)
-        root.title('Krzysztof Kosinski - program grafika komputerowa')
-        root.minsize(width=800, height=600)
 
-        line = Button(root, width=15, command=1, text='Linia', height=2)
-        line.grid(row=0, column=1, sticky='N', padx=10, pady=10)
+        self.root.configure(bg=maincolor)
+        self.root.title('Krzysztof Kosinski - program grafika komputerowa')
+        self.root.minsize(width=800, height=600)
 
-        rectangle = Button(root, width=15, command=1, text='Prostokąt', height=2)
-        rectangle.grid(row=1, column=1, sticky='N', padx=10, pady=10)
+        line_prompted = Button(self.root, width=15, command=self.line_prompt, text='Linia - tekstowe', height=2)
+        line_prompted.grid(row=0, column=1, sticky='N')
 
-        circle = Button(root, width=15, command=1, text='Okrąg', height=2)
-        circle.grid(row=2, column=1, sticky='N', padx=10, pady=10)
+        rectangle_prompted = Button(self.root, width=15, command=1, text='Prostokąt - tekstowe', height=2)
+        rectangle_prompted.grid(row=1, column=1, sticky='N')
 
-        w = Canvas(root,
-                   width=700,
-                   height=600,
-                   background='#ffffff')
+        circle_prompted = Button(self.root, width=15, command=1, text='Okrąg - tekstowe', height=2)
+        circle_prompted.grid(row=2, column=1, sticky='N')
 
-        w.grid(row=0, column=2, columnspan=2, rowspan=3, sticky=W+E+N+S)
+        line_mouse = Button(self.root, width=15, command=self.line_mouse, text='Linia - mysz', height=2)
+        line_mouse.grid(row=3, column=1, sticky='N', padx=10, pady=10)
 
-        # w.pack(expand=YES, fill=BOTH)
-        # w.bind("<B1-Motion>", paint)
+        rectangle_mouse = Button(self.root, width=15, command=1, text='Prostokąt - mysz', height=2)
+        rectangle_mouse.grid(row=4, column=1, sticky='N', padx=10, pady=10)
 
-        root.mainloop()
+        circle_mouse = Button(self.root, width=15, command=1, text='Okrąg - mysz', height=2)
+        circle_mouse.grid(row=5, column=1, sticky='N', padx=10, pady=10)
+
+
+        # paint = Button(self.root, width=15, command=self.paint, text='Rysowanko', height=2)
+        # paint.grid(row=2, column=1, sticky='N', padx=10, pady=10)
+
+        self.w.grid(row=0, column=2, columnspan=2, rowspan=3, sticky=W+E+N+S)
+
+        self.root.mainloop()
+
+    def line_prompt(self):
+        r = Tk()
+        r.title('Wprowadz dane')
+        r.geometry('600x150')
+        Label(r, text="X poczatkowe").grid(column=1, row=0, sticky=W)
+        Label(r, text="Y poczatkowe").grid(column=1, row=1, sticky=W)
+        Label(r, text="X koncowe").grid(column=3, row=0, sticky=W)
+        Label(r, text="Y koncowe").grid(column=3, row=1, sticky=W)
+
+        x_start_entry = Entry(r)
+        y_start_entry = Entry(r)
+        x_end_entry = Entry(r)
+        y_end_entry = Entry(r)
+
+        x_start_entry.grid(row=0, column=2)
+        y_start_entry.grid(row=1, column=2)
+        x_end_entry.grid(row=0, column=4)
+        y_end_entry.grid(row=1, column=4)
+
+        draw_line_button = Button(r, command=lambda: self.line(x_start_entry.get(), y_start_entry.get(), x_end_entry.get(), y_end_entry.get()), text='Rysuj')
+        draw_line_button.grid(columnspan=4, row=2, column=1, padx=10, pady=10)
+
+    def line_mouse(self):
+        self.w.bind("<Button-1>", self.line_mouse_clicked)
+
+    def line_mouse_clicked(self, event):
+        if not self.mouse_line_start:
+            self.mouse_line_start = {
+                'x': event.x,
+                'y': event.y
+            }
+        else:
+            self.mouse_line_end = {
+                'x': event.x,
+                'y': event.y
+            }
+            self.line(self.mouse_line_start['x'], self.mouse_line_start['y'], self.mouse_line_end['x'], self.mouse_line_end['y'])
+            self.mouse_line_start = None
+            self.mouse_line_end = None
+
+    def line(self, x_start, y_start, x_end, y_end):
+        self.w.create_line(x_start, y_start, x_end, y_end)
+
+    def paint(self):
+        if self.mode is not 'line':
+            self.w.bind("<B1-Motion>", self.start_paint)
+            self.mode = 'line'
+        else:
+            self.w.unbind("<B1-Motion>")
+            self.mode = 'none'
+
+    def rectangle(self):
+        test = 1;
+
+
+    def start_paint(self, event):
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.w.create_oval(x1, y1, x2, y2, fill='#000000')
 
 
 Menu()
-
-# def paint(event):
-#     python_green = "#476042"
-#     x1, y1 = (event.x - 1), (event.y - 1)
-#     x2, y2 = (event.x + 1), (event.y + 1)
-#     w.create_oval(x1, y1, x2, y2, fill=python_green)
-#
-#
-# master = Tk()
-# master.title("Painting using Ovals")
-# w = Canvas(master,
-#            width=canvas_width,
-#            height=canvas_height)
-# w.pack(expand=YES, fill=BOTH)
-# w.bind("<B1-Motion>", paint)
-#
-# message = Label(master, text="Press and Drag the mouse to draw")
-# message.pack(side=BOTTOM)
-#
-# mainloop()
-#
-# print('x')

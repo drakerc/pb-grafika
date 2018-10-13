297e37691e35709d2e9b1e6c53c916d872393dd0
Krzysztof Kosinski <drakerc@me.com>
2018-10-13 14:25:44 +0200

    Add JPG loading and saving

--- zad2.py
+++ zad2.py
@@ -29,15 +29,15 @@ class PPM:
             if not self.header_read:
                 self.read_header(value)
                 continue
-            if self.type == 1:
-                print('The P6/P3 header does not match its real type')
-                exit()
+            # if self.type == 1:
+            #     print('The P6/P3 header does not match its real type')
+            #     exit()
 
             if self.img is None:
                 self.img = Image.new('RGB', (self.width, self.height), "black")  # create a new black image
                 self.pixels = self.img.load()  # create the pixel map
 
-            self.read_pixels(value)
+            self.read_pixels(value, )
 
     def read_ppm_binary(self, file, chunk_size):
         while not self.header_read:
@@ -97,6 +97,10 @@ class PPM:
     def read_pixels(self, value):
         line_elements = value.split()
         for element in line_elements:
+            if element.startswith('#') or element == '':
+                continue
+            element = element.split('#')[0]  # remove any comments that are not in the beginning
+
             if self.pixel_r is None:
                 self.pixel_r = int(element)
                 continue
@@ -135,6 +139,8 @@ class Menu:
                height=700,
                background='#ffffff')
 
+    image = None
+
     def __init__(self):
         maincolor = '#0288d1'
 
@@ -144,6 +150,12 @@ class Menu:
 
         load_ppm = Button(self.root, width=15, command=self.load_ppm, text='Wczytaj plik PPM', height=2)
         load_ppm.grid(row=0, column=1, sticky='N')
+
+        load_jpg= Button(self.root, width=15, command=self.load_jpg, text='Wczytaj plik JPG', height=2)
+        load_jpg.grid(row=1, column=1, sticky='N')
+
+        save_jpg= Button(self.root, width=15, command=self.save_jpg_prompt, text='Zapisz plik JPG', height=2)
+        save_jpg.grid(row=2, column=1, sticky='N')
 
         self.w.grid(row=0, column=2, columnspan=2, rowspan=9, sticky=W+E+N+S)
 
@@ -181,11 +193,55 @@ class Menu:
             ppm_object.read_ppm_binary(file, self.CHUNKSIZE)
             file.close()
 
-        image = self.resize_image(ppm_object.get_image())
+        self.image = self.resize_image(ppm_object.get_image())
         print("--- %s seconds ---" % (time.time() - start_time))
-        tkimage = ImageTk.PhotoImage(image)
+        tkimage = ImageTk.PhotoImage(self.image)
         self.w.create_image(350, 350, image=tkimage)
         self.root.mainloop()
+
+    def load_jpg(self):
+        file = askopenfilename()
+        filename, file_extension = os.path.splitext(file)
+        if file_extension != '.jpg' and file_extension != '.jpeg':
+            print('Wrong file selected')
+            exit()
+
+        self.image = self.resize_image(Image.open(file))
+        tkimage = ImageTk.PhotoImage(self.image)
+        self.w.create_image(350, 350, image=tkimage)
+        self.root.mainloop()
+
+    def save_jpg_prompt(self):
+        if self.image is None:
+            print('No images available!')
+            return
+
+        r = Tk()
+        r.title('Wprowadz dane')
+        r.geometry('600x300')
+        Label(r, text="Kompresja").grid(column=1, row=0, sticky=W)
+        Label(r, text="Nazwa pliku").grid(column=1, row=1, sticky=W)
+
+        compression = Scale(r, from_=0, to=100, orient=HORIZONTAL)
+        file_name = Entry(r)
+
+        compression.set(90)
+        file_name.insert(0, 'output.jpg')
+
+        compression.grid(row=0, column=2)
+        file_name.grid(row=1, column=2)
+
+        save_button = Button(r, command=lambda: self.save_jpg(compression.get(), file_name.get()), text='Zapisz')
+        save_button.grid(columnspan=4, row=2, column=1, padx=10, pady=10)
+
+    def save_jpg(self, compression, file_name):
+        self.image.save(file_name, quality=compression)
+        r = Tk()
+        r.configure(bg='#00ff00')
+        r.title('Sukces')
+        r.geometry('350x50')
+        rlbl = Label(r, text='\nPomyslnie zapisano plik.',)
+        rlbl.pack()
 
 
 Menu()
